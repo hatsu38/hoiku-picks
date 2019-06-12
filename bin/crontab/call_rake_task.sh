4fe717bf20f81b32758c68276ebde1ab6df0f89d
toywonder <toy.wonder70@gmail.com>
2019-06-12 15:50:54 +0900

    :bug:bundleの変数を解除

--- bin/crontab/call_rake_task.sh
+++ bin/crontab/call_rake_task.sh
@@ -9,8 +9,6 @@ fi
 
 if [ "$ARG_RAILS_ENV" = "production" ]; then
   cd /var/www/hoiku-picks/current/
-else
-  BUNDLE=bundle
 fi
 
-RAILS_ENV=$ARG_RAILS_ENV $BUNDLE exec rake ${ARG_RAKE_COMMAND}
+RAILS_ENV=$ARG_RAILS_ENV bundle exec rake ${ARG_RAKE_COMMAND}
