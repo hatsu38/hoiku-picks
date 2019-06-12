2845801540f54ca48136eec6c1783cd5a74dbe19
toywonder <toy.wonder70@gmail.com>
2019-06-12 11:45:20 +0900

    :bug:環境を第一引数として設定

--- bin/crontab/call_rake_task.sh
+++ bin/crontab/call_rake_task.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-ARG_RAKE_COMMAND=$1
+ARG_RAILS_ENV=$1
+ARG_RAKE_COMMAND=$2
 
-$HOME/.rbenv/bin/rbenv exec bundle exec rake ${ARG_RAKE_COMMAND}
+if [ -z "$ARG_RAILS_ENV" ]; then
+  echo "実行するRAILS_ENVを引数に与えてください"
+  exit 1
+fi
+
+if [ "$ARG_RAILS_ENV" = "production" ]; then
+  cd /var/www/hoiku-picks/current/
+else
+  BUNDLE=bundle
+fi
+
+RAILS_ENV=$ARG_RAILS_ENV $BUNDLE exec rake ${ARG_RAKE_COMMAND}
